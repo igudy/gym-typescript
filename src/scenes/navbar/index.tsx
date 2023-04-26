4867e118924bc8b7707bfe637f2d82e46d64ef97
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-04-26 17:14:59 +0100

    Assets added, working on navbar

---
+++ src/scenes/navbar/index.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+import Logo from "@/assets/Logo.png";
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+    const flexBetween = "flex items-center justify-between";
+    return (
+      <nav>
+          <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>Navbar</div>
+      </nav>
+  )
+}
+export default Navbar;
